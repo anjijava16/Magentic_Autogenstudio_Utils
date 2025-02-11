f24de0070ee1da7040f2ca58f8daca01e5d6fe60
Anjaiah Methuku <anjijava16@users.noreply.github.com>
2025-02-10 23:30:48 -0500

    Add files via upload

---
+++ tool_load/cust_tools.py
@@ -0,0 +1,9 @@
+# app/process/tools/cust_tools.py
+
+def get_weather(city: str) -> str:
+    """Get the weather for a city."""
+    return f"The weather in {city} is 73 degrees and Sunny."
+
+def get_temperature(city: str) -> str:
+    """Get the weather for a city."""
+    return f"The weather in {city} is 73 degrees and Sunny."
